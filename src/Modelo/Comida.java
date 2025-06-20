76443b9b2ae4ad78aa4e259319e9504e99596cff
Jhon Olarte <deibyolarte24@gmail.com>
2025-06-20 08:19:08 -0500

    Agrego el modelo de la Comida

--- src/Modelo/Comida.java
+++ src/Modelo/Comida.java
@@ -1,5 +1,37 @@
 package Modelo;
 
 public class Comida {
-
+	private int id;
+	private String nombre;
+	private String descripcion;
+	private int nivelDificultad;
+	private int tiempoEstimado;
+	private String rutaImagen;
+	
+	public Comida (int id, String nombre, String descripcion, int nivelDificultad, int tiempoEstimado, String rutaImagen) {
+		this.id = id;
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		this.nivelDificultad = nivelDificultad;
+		this.tiempoEstimado = tiempoEstimado;
+		this.rutaImagen = rutaImagen;
+	}
+	
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    
+    public String getNombre() { return nombre;}
+    public void setNombre(String nombre) {this.nombre = nombre;}
+    
+    public String getDescripcion() {return descripcion;}
+    public void setDescripcion(String descripcion) {this.descripcion = descripcion;}
+    
+    public int getNivelDificultad() {return nivelDificultad;}
+    public void setNivelDificultad (int nivelDificultad) {this.nivelDificultad = nivelDificultad;}
+    
+    public int getTiempoEstimado() {return tiempoEstimado;}
+    public void setTiempoEstimado(int tiempoEstimado) {this.tiempoEstimado = tiempoEstimado;}
+    
+    public String getRutaImagen() {return rutaImagen;}
+    public void setRutaImagen(String rutaImagen) {this.rutaImagen = rutaImagen;}
 }
--- src/Modelo/Conexion.java
+++ src/Modelo/Conexion.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 public class Conexion {
     
-    private static final String URL = "jdbc:mysql://localhost:3306/crud_cocina"; // Cambia el puerto o base si es necesario
-    private static final String USUARIO = "root";   // Tu usuario de MySQL
-    private static final String CLAVE = "";         // Tu contraseña (vacía si no tiene)
+    private static final String URL = "jdbc:mysql://localhost:3306/crud_cocina";
+    private static final String USUARIO = "root";
+    private static final String CLAVE = "";
 
     public static Connection getConexion() {
         try {
